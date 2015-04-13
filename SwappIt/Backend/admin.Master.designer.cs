39766bdaa53fa634af748dc2aae3130608d7cdb6
Mikail Tolga Turan <Turan1102@hotmail.com>
2015-04-13 02:12:26 +0200

    Linkbutton placeholder oprettet

--- SwappIt/Backend/admin.Master.designer.cs
+++ SwappIt/Backend/admin.Master.designer.cs
@@ -29,5 +29,23 @@ namespace Backend {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
+        
+        /// <summary>
+        /// ContentPlaceHolderWithLinkButton control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolderWithLinkButton;
+        
+        /// <summary>
+        /// test control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder test;
     }
 }

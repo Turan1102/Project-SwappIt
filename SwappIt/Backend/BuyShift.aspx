2f4cdcfb216836550a314caa20e13bed4f0799f0
Mykremin <mykremin_93@hotmail.com>
2015-08-26 18:36:41 +0200

    BuyShift side mangler at lave købe knappen færdig :-)

---
+++ SwappIt/Backend/BuyShift.aspx
@@ -0,0 +1,72 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/admin.Master" AutoEventWireup="true" CodeBehind="BuyShift.aspx.cs" Inherits="Backend.BuyShift" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="pagetitle" runat="server">
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="row">
+        <div class="col-md-12">
+            <!-- BEGIN TABLE PORTLET-->
+            <div class="portlet box blue">
+                <div class="portlet-title">
+							<div class="caption">
+								<i class="fa fa-shopping-cart"></i>Køb vagt
+							</div>
+						</div>
+                <div class="portlet-body">
+
+                    <div class="table-responsive">
+                        <table id="dataTable" class="table table-striped table-bordered table-hover dataTable">
+                            <thead>
+                                <tr>
+                                    <th><i class="fa fa-user"></i>Sælges af</th>
+                                    <th><i class="fa fa-calendar"></i>Vagtdato</th>
+                                    <th><i class="fa fa-clock-o"></i>Tid</th>
+                                    <th><i class="fa fa-shopping-cart"></i>Køb</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <asp:Literal ID="tableOut" runat="server"></asp:Literal>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+            <!-- END TABLE PORTLET-->
+        </div>
+    </div>
+            
+</asp:Content>
+<asp:Content ID="Content4" ContentPlaceHolderID="ContentPlaceHolder2" runat="server">
+</asp:Content>
+<asp:Content ID="Content5" ContentPlaceHolderID="ContentPlaceHolderWithLinkButton" runat="server">
+</asp:Content>
+<asp:Content ID="Content6" ContentPlaceHolderID="test" runat="server">
+</asp:Content>
+<asp:Content ID="Content7" ContentPlaceHolderID="scripts" runat="server">
+      <script>
+
+
+        jQuery(document).ready(function () {
+
+            var oTable = $('#dataTable').dataTable({
+                "bPaginate": false,
+                "bFilter": false,
+                "bSort": true,
+                "bInfo": false,
+                "aoColumns": [
+                                null,
+                                { "sType": "datetime-eu" },
+                                { "bSortable": false },
+                                { "bSortable": false }
+                ]
+            });
+
+            Helpers.init();
+
+            <%= script %>
+        });
+
+
+    </script>
+</asp:Content>
---
+++ SwappIt/Backend/BuyShift.aspx.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Backend.code;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Backend
+{
+    public partial class BuyShift : AdminPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Request["BuyShiftId"] != null)
+            {
+                Buy(Request["BuyShiftId"].ToString());
+            }
+
+            Fill();            
+
+        }
+
+        private void Fill()
+        {
+            tableOut.Text = "";
+
+            List<SqlParameter> p = new List<SqlParameter>();
+            p.Add(new SqlParameter("SIID", ui.Siid)); // skal vi evt. tjekke om s.inactive = 0????
+            DataTable dt = db.GetDataSet("SELECT e.Firstname, e.Middlename, e.Lastname, s.IID, s.Date, s.StartTime, s.EndTime FROM Employee e, Shift s WHERE s.EIID = e.IID AND s.SIID=@SIID AND s.Type=0 AND s.Inactive=0 ORDER BY Date;", p).Table;
+
+            foreach (DataRow r in dt.Rows)
+            {
+                tableOut.Text += "<tr>" +
+                            "   <td>" + r["Firstname"].ToString() + " " + (r["Middlename"].ToString() != "" ? (r["Middlename"].ToString()) + " " : "") + r["Lastname"].ToString() + "</td>" +
+                            "   <td>" + r["Date"].ToString() + "</td>" +
+                            "   <td>" + r["StartTime"].ToString() + " - " + r["EndTime"].ToString() + "</td>" +
+                            "   <td>" + "<a href=\"BuyShift.aspx?BuyShiftId=" + r["IID"] + "\" class=\"btn default btn-xs green btn-confirm\"><i class=\"fa fa-shopping-cart\"></i> Køb</a>" + "</td>" +
+                                "</tr>";
+            }
+        }
+
+        private void Buy(string id)
+        {
+            if (CheckIfBuyingOwnShift(id))
+            {
+                List<SqlParameter> p = new List<SqlParameter>();
+                p.Add(new SqlParameter("ShiftID", id));
+                p.Add(new SqlParameter("EIID", ui.Id));
+                string SQL1 = "UPDATE Shift SET Inactive=1 WHERE IID=@ShiftID";
+                db.ExecuteUpdate(SQL1, p);
+
+                string SQL2 = "INSERT INTO BuyShiftComplete (ShiftId, BuyerEIID) VALUES (@ShiftID, @EIID)";
+                db.ExecuteInsert(SQL2, p);  
+            }
+
+            // fyr en fejlbesked popup af med at man køber egen vagt
+        }
+
+        private Boolean CheckIfBuyingOwnShift(string IID)
+        {
+            List<SqlParameter> p = new List<SqlParameter>();
+            p.Add(new SqlParameter("IID", IID));
+            DataTable dt = db.GetDataSet("SELECT EIID FROM Shift WHERE IID=@IID;", p).Table;
+
+            if (dt.Rows.Count > 0)
+            {
+                return false;
+            }
+
+            return true; 
+        }
+    }
+}
